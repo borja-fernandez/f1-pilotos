cfac460bca17dd5a4bacdd1ebad53fa01e11b32b
Borja Fernandez <borja.fernandez@outlook.com>
2022-10-30 10:37:43 +0100

    first commit
    1st commit

---
+++ src/main/java/es/f1/pilotos/PilotosApplication.java
@@ -0,0 +1,13 @@
+package es.f1.pilotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/f1/pilotos/PilotosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.f1.pilotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
