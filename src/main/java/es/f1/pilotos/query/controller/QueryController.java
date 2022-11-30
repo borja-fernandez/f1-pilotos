fb9b6ec5277f4813503f609fca6b7ac3c8bcf676
Borja Fernandez <borja.fernandez@outlook.com>
2022-11-30 16:58:10 +0100

    Include GetPiloto in QueryController

--- src/main/java/es/f1/pilotos/query/controller/QueryController.java
+++ src/main/java/es/f1/pilotos/query/controller/QueryController.java
@@ -1,6 +1,12 @@
 package es.f1.pilotos.query.controller;
 
 import es.f1.pilotos.PilotosApplication;
+import es.f1.pilotos.command.exceptions.ObjectNotFoundException;
+import es.f1.pilotos.query.service.PilotoQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +16,16 @@ public class QueryController {
     private static final String API_VERSION = "/v1";
     private static final String API_PATH = PilotosApplication.PILOTOS_QUERY_PATH + API_VERSION + "/piloto";
 
+    @Autowired
+    PilotoQueryService pilotoQueryService;
+
     @GetMapping(value = API_PATH + "/{id}")
-    public String getPiloto(@PathVariable String id){
-        return "Piloto: " +  id;
+    public ResponseEntity getPiloto(@PathVariable String id){
+        return ResponseEntity.ok(pilotoQueryService.recuperarPiloto(id));
+    }
+
+    @ExceptionHandler({ ObjectNotFoundException.class  })
+    public ResponseEntity manejarObjectNotFoundException(RuntimeException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/es/f1/pilotos/query/service/PilotoQueryService.java
+++ src/main/java/es/f1/pilotos/query/service/PilotoQueryService.java
@@ -1,5 +1,6 @@
 package es.f1.pilotos.query.service;
 
+import es.f1.pilotos.command.exceptions.ObjectNotFoundException;
 import es.f1.pilotos.query.repository.Piloto;
 import es.f1.pilotos.query.repository.PilotoQueryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,13 @@ public class PilotoQueryService {
     @Autowired
     PilotoQueryRepo pilotoQueryRepo;
 
-    public Optional<Piloto> recuperarPiloto(String codigo){
-        return pilotoQueryRepo.findByCodigo(codigo);
-    }
-    public void eliminarTodosPilotos(){
-        pilotoQueryRepo.deleteAll();
+    public Piloto recuperarPiloto(String codigo){
+        Optional<Piloto> piloto = pilotoQueryRepo.findByCodigo(codigo);
+
+        if(!piloto.isPresent()) {
+            throw new ObjectNotFoundException("Piloto " + codigo + " no encontrado");
+        }
+        return piloto.get();
     }
 
     public void eliminarPiloto(String codigo){
